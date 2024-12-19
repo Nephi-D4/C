b390014be19e63a0e36aee7d13140421bcb16522
Nephi-D4 <313038@s.lhvs.tyc.edu.tw>
2024-12-19 15:21:23 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -12,5 +12,14 @@ void myTime::printTime(){
    count<<hour<<":"<<minute<<"/n";
 }
 int mail(){
+    myTime now, open;
+    now.hour = 8;
+    now.minute = 18;
+    count<<"現在時間:";
+    now.printTime();
+    open.minute = 10
+    cout<<"開張時間:";
+    open.printTime();
+}
 	
 }
